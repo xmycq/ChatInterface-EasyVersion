620e1e92d6b5a99bcf588ec664638152ea2a1f00
xumingyao <2503542032@qq.com>
2020-11-21 20:48:06 +0800

    to test

--- app/src/main/java/com/example/uibestpractice/test.java
+++ app/src/main/java/com/example/uibestpractice/test.java
@@ -2,6 +2,7 @@ package com.example.uibestpractice;
 
 public class test {
     private int test;
+    private String test2;
 
     public int getTest() {
         return test;
@@ -9,5 +10,13 @@ public class test {
 
     public void setTest(int test) {
         this.test = test;
+    }
+
+    public String getTest2() {
+        return test2;
+    }
+
+    public void setTest2(String test2) {
+        this.test2 = test2;
     }
 }
