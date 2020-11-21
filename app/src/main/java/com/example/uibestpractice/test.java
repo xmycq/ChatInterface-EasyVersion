494ff085499961ae711c77bffbc4181885c17035
xumingyao <2503542032@qq.com>
2020-11-21 20:44:49 +0800

    to test

---
+++ app/src/main/java/com/example/uibestpractice/test.java
@@ -0,0 +1,13 @@
+package com.example.uibestpractice;
+
+public class test {
+    private int test;
+
+    public int getTest() {
+        return test;
+    }
+
+    public void setTest(int test) {
+        this.test = test;
+    }
+}
